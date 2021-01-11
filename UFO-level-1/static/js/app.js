4667fa962eab671e2cb3cfbcd36a822dd0d51d25
Marina Ercoli <68237165+Maercoli@users.noreply.github.com>
2021-01-11 09:49:12 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,7 +4,12 @@ var tbody = d3.select("tbody");
 // Console.log the UFO data from data.js
 console.log(data);
 
-//Loop Through `data` and console.log each UFO report object in the table
+//The code below uses the Arrow function to:
+// 1- Loop Through `data` and console.log each UFO report object
+// 2- Use d3 to append one table row `tr` for each UFO report object
+// 3- Use d3 to append 1 cell per UFO report value 
+// 4- Use d3 to update each cell's text with UFO report values (datetime, city, state, country, shape, durationMinutes, comments)
+
 data.forEach((ufoReport) => {
     var row = tbody.append("tr");
     Object.entries(ufoReport).forEach(([key,value]) => { 
@@ -12,3 +17,5 @@ data.forEach((ufoReport) => {
     cell.text(value)
     });
 });
+
+//
